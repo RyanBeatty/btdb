381ab4727d5257dad2f49875422514b852c834ba
Ryan Beatty <rvbeatty@email.wm.edu>
2019-02-02 11:52:10 -0800

    getting a feel for rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+
+
+mod panda_db {
+    
+    #[derive(Debug)]
+    pub struct DB {
+        table: std::collections::HashMap<String, String>
+    }
+
+    impl DB {
+        pub fn new() -> DB {
+            return DB {
+                table: std::collections::HashMap::new()
+            }
+        }
+
+        pub fn set(&mut self, key : String, val : String) {
+            self.table.insert(key, val);
+        }
+
+        pub fn get(&self, key : String) -> std::option::Option<&String> {
+            return self.table.get(&key);
+        }
+
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut db = panda_db::DB::new();
+    db.set(String::from("hello"), String::from("World"));
+    println!("Created the db: {:?}", db);
+    println!("Get query: {:?}", db.get(String::from("hello")));
 }
