1f5383c55a0709b5186190d959895604d8c89762
Ryan Beatty <rvbeatty@email.wm.edu>
2019-02-21 22:02:55 -0800

    starting writing prompt loop interface

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-
+use std::io::Write;
 
 mod panda_db;
 
 fn main() {
-    println!("Hello, world!");
+    println!("BTDB Version 0.1.0");
+    let mut buffer = String::new();
+    loop {
+        print!("btdb> ");
+        std::io::stdout().flush().unwrap();
 
-    let mut db = panda_db::DB::new();
-    db.set(String::from("hello"), String::from("World"));
-    println!("Created the db: {:?}", db);
-    println!("Get query: {:?}", db.get(String::from("hello")));
+        buffer.clear();
+        match std::io::stdin().read_line(&mut buffer) {
+            Err(error) => {
+                println!("Error reading input: {}", error);
+                continue;
+            },
+            _ => (),
+        }
+
+        let line = buffer.trim_end();
+
+        match line {
+            "\\q" => break,
+            _ => println!("Entered: {}", line),
+        }
+
+    }
 }
