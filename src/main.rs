0c7a75ef1609a37848eaf435de3a9b35f3360fd7
Ryan Beatty <rvbeatty@email.wm.edu>
2019-02-23 17:29:42 -0800

    fleshing out repl skeleton a bit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use std::error;
 use std::io::Write;
 
 mod panda_db;
+
+enum Command {
+    Unknown,
+    Quit,
+    Insert{ key: String, val: String},
+}
 
 fn main() {
     println!("BTDB Version 0.1.0");
@@ -20,10 +27,25 @@ fn main() {
 
         let line = buffer.trim_end();
 
-        match line {
-            "\\q" => break,
-            _ => println!("Entered: {}", line),
+        let mut command = Command::Unknown;
+        if line.starts_with("\\") {
+            match line.as_ref() {
+                "\\q" => command = Command::Quit,
+                _     => (),
+            }
+        } else {
+            if line.starts_with("insert") {
+                let splt : Vec<&str> = line.split(" ").collect();
+                if splt.len() == 3 {
+                    command = Command::Insert{key: splt[1].to_string(), val: splt[2].to_string()};
+                }
+            }
         }
 
+        match command {
+            Command::Unknown => println!("Unknown Command: {}", line),
+            Command::Quit    => break,
+            Command::Insert{key, val}  => println!("Insert command key={} val={}", key, val),
+        }
     }
 }
