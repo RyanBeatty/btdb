e7b48f69766c11f7ce40725fc9b914f4590f3e04
Ryan Beatty <rvbeatty@email.wm.edu>
2019-03-19 17:57:24 -0700

    added sqlparser dep and rigged up a main loop to test what it produces

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ extern crate quick_error;
 
 use std::io::Write;
 
+use sqlparser::dialect::PostgreSqlDialect;
+use sqlparser::sqlparser::*;
+
 use btdb;
 
 // enum Command {
@@ -57,43 +60,23 @@ use btdb;
 // }
 
 fn main() {
-    // println!("BTDB Version 0.1.0");
-    // let mut db = btdb::DB::open_db().unwrap();
-    // let mut buffer = String::new();
-    // loop {
-    //     print!("btdb> ");
-    //     std::io::stdout().flush().unwrap();
+    println!("BTDB Version 0.1.0");
+    loop {
+        print!("btdb> ");
+        std::io::stdout().flush().unwrap();
 
-    //     buffer.clear();
-    //     match std::io::stdin().read_line(&mut buffer) {
-    //         Err(error) => {
-    //             println!("Error reading input: {}", error);
-    //             continue;
-    //         }
-    //         _ => (),
-    //     }
-
-    //     let line = buffer.trim_end();
-    //     match parse_command(line) {
-    //         Err(err) => println!("Error: {}", err),
-    //         Ok(Command::Unknown) => println!("Uknown command"),
-    //         Ok(Command::Quit) => break,
-    //         Ok(Command::Insert { id, foo }) => match db.insert(btdb::Tuple { id: id, foo: foo }) {
-    //             Err(e) => println!("Error: {}", e),
-    //             Ok(_) => println!("Inserted"),
-    //         },
-    //         Ok(Command::Select { id }) => {
-    //             let results: Vec<prettytable::Row> = db
-    //                 .select(id)
-    //                 .unwrap()
-    //                 .iter()
-    //                 .map(|tuple| row![tuple.id, tuple.foo])
-    //                 .collect();
-    //             let mut table = prettytable::Table::init(results);
-    //             table.set_titles(row!["id", "foo"]);
-    //             print!("{}", table);
-    //             println!("({} rows)\n", table.len());
-    //         }
-    //     }
-    // }
+        let mut buffer = String::new();
+        std::io::stdin().read_line(&mut buffer).unwrap();
+        if buffer.trim() == "\\q" {
+            break;
+        }
+        let ast = match Parser::parse_sql(&PostgreSqlDialect{}, buffer) {
+            Err(err) => {
+                println!("Failed to parse sql input: {:?}", err);
+                continue;
+            },
+            Ok(ast) => ast,
+        };
+        println!("ast: {:?}", ast);
+    }
 }
