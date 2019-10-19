b21659dc38421cc3a92233107ba6178fe9c47871
Ryan Beatty <rvbeatty@email.wm.edu>
2019-10-19 10:23:27 -0700

    cleanup

--- src/sql/main.cpp
+++ src/sql/main.cpp
@@ -4,15 +4,13 @@
 #include "node.hpp"
 
 int main() {
-  // btdb::sql::NIdentifier id("foo");
-  // btdb::sql::NExpr expr = id;
-  // btdb::sql::PrintParseTreeVisitor pp(expr);
-  // std::cout << pp.PrettyPrint() << std::endl;
   for (std::string line; std::getline(std::cin, line);) {
     btdb::sql::ParserContext ctx(line);
-    ctx.Parse();
-    btdb::sql::PrintContext print_ctx;
-    btdb::sql::print_parse_node(ctx.tree->tree, print_ctx);
-    std::cout << print_ctx.Print() << std::endl;
+    auto result = ctx.Parse();
+    if (result == 0) {
+      btdb::sql::PrintContext print_ctx;
+      btdb::sql::print_parse_node(ctx.tree->tree, print_ctx);
+      std::cout << print_ctx.Print() << std::endl;
+    }
   }
 }
