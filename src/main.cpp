18fcd2299c7cfc1824f5402ccccea5a1a2fda672
Ryan Beatty <rvbeatty@email.wm.edu>
2019-11-30 18:06:34 -0800

    remove unused imports

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,6 @@
-#include <arpa/inet.h>
-#include <errno.h>
-#include <error.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <algorithm>
 #include <iostream>
 #include <iterator>
