d1bcb900c7dcd01c59b15f9e054de81298f3df5c
Ryan Beatty <rvbeatty@email.wm.edu>
2019-12-03 19:57:15 -0800

    remove iterator classes

--- src/main.cpp
+++ src/main.cpp
@@ -14,22 +14,6 @@
 #include "types.h"
 #include "utils.h"
 
-struct Iterator {
-  virtual void Open() = 0;
-  virtual Tuple* GetNext() = 0;
-  virtual void Close() = 0;
-};
-
-// struct SequentialIterator : Iterator {
-//   SequentialScan scan;
-//   SequentialIterator() {}
-//   void Open() { scan.Open(); }
-//   MTuple GetNext() { return scan.GetNext(); }
-//   void Close() { return scan.Close(); }
-// };
-
-typedef Iterator Plan;
-
 Datum EvalExpr(ParseNode* node, Tuple* cur_tuple) {
   switch (node->type) {
     case NSTRING_LIT: {
@@ -196,168 +180,141 @@ Datum EvalExpr(ParseNode* node, Tuple* cur_tuple) {
   }
 }
 
-struct SequentialScan : Iterator {
+typedef struct ScanState {
   uint64_t next_index;
-
   CharPtrVec* target_list;
-  // TODO(ryan): Memory will be deallocated in ParseTree desctructor. Figure out how to handle
-  // ownership transfer eventually.
   ParseNode* where_clause;
-
-  SequentialScan(CharPtrVec* target_list, ParseNode* where_clause)
-      : next_index(0), target_list(target_list), where_clause(where_clause) {}
-
-  void Open() {}
-  Tuple* GetNext() {
-    for (;;) {
-      Tuple* cur_tpl = GetTuple(next_index);
-      if (cur_tpl == NULL) {
-        return NULL;
-      }
-      ++next_index;
-
-      // Evaluate predicate if any.
-      if (where_clause != NULL) {
-        Datum result_val = EvalExpr(where_clause, cur_tpl);
-        assert(result_val.type == T_BOOL);
-        assert(result_val.data != NULL);
-        bool* result = (bool*)result_val.data;
-        if (!*result) {
-          continue;
-        }
-      }
-
-      // Column projections.
-      Tuple* result_tpl = NULL;
-      CharPtrVecIt target = NULL;
-      VEC_FOREACH(target, target_list) {
-        Datum* data = GetCol(cur_tpl, *target);
-        assert(data != NULL);
-        result_tpl = SetCol(result_tpl, *target, *data);
-      }
-      return result_tpl;
-    }
-  }
-  void Close() {}
-};
-
-struct InsertScan : Iterator {
-  Tuple** tuples;
-
-  InsertScan(Tuple** tuples) : tuples(tuples) {}
-
-  void Open() {}
-
-  Tuple* GetNext() {
-    Tuple* cur_tuple = NULL;
-    for (size_t i = 0; i < arrlen(tuples); ++i) {
-      cur_tuple = tuples[i];
-      assert(cur_tuple != NULL);
-      InsertTuple(CopyTuple(cur_tuple));
-    }
-    return NULL;
-  }
-
-  void Close() {}
-};
-
-struct DeleteScan : Iterator {
-  uint64_t next_index;
-  ParseNode* where_clause;
-
-  DeleteScan(ParseNode* where_clause) : next_index(0), where_clause(where_clause) {}
-
-  void Open() {}
-
-  Tuple* GetNext() {
-    for (;;) {
-      Tuple* cur_tpl = GetTuple(next_index);
-      if (cur_tpl == NULL) {
-        return NULL;
-      }
-
-      // Evaluate predicate if any.
-      if (where_clause != NULL) {
-        Datum result_val = EvalExpr(where_clause, cur_tpl);
-        assert(result_val.type == T_BOOL);
-        assert(result_val.data != NULL);
-        bool* result = (bool*)result_val.data;
-        if (!*result) {
-          ++next_index;
-          continue;
-        }
-      }
-
-      Tuple* new_tuple = CopyTuple(cur_tpl);
-      DeleteHeapTuple(next_index);
-      return new_tuple;
-    }
-
-    return NULL;
-  }
-
-  void Close() {}
-};
-
-struct UpdateScan : Iterator {
-  uint64_t next_index = 0;
-
+  Tuple** tuples;  // stb_arr
   List* assign_exprs;
-  // TODO(ryan): Memory will be deallocated in ParseTree desctructor. Figure out how to handle
-  // ownership transfer eventually.
-  ParseNode* where_clause;
 
-  UpdateScan(List* assign_exprs, ParseNode* where_clause)
-      : assign_exprs(assign_exprs), where_clause(where_clause) {}
+  Tuple* (*scan_func)(ScanState*);
+} ScanState;
 
-  void Open() {}
-  Tuple* GetNext() {
-    for (;;) {
-      Tuple* cur_tpl = GetTuple(next_index);
-      if (cur_tpl == NULL) {
-        return NULL;
+ScanState* MakeScanState(CharPtrVec* target_list, ParseNode* where_clause, Tuple** tuples,
+                         List* assign_exprs, Tuple* (*scan_func)(ScanState*)) {
+  ScanState* state = (ScanState*)calloc(1, sizeof(ScanState));
+  state->next_index = 0;
+  state->target_list = target_list;
+  state->where_clause = where_clause;
+  state->tuples = tuples;
+  state->assign_exprs = assign_exprs;
+  state->scan_func = scan_func;
+  return state;
+}
+
+Tuple* SequentialScan(ScanState* state) {
+  for (;;) {
+    Tuple* cur_tpl = GetTuple(state->next_index);
+    if (cur_tpl == NULL) {
+      return NULL;
+    }
+    ++state->next_index;
+
+    // Evaluate predicate if any.
+    if (state->where_clause != NULL) {
+      Datum result_val = EvalExpr(state->where_clause, cur_tpl);
+      assert(result_val.type == T_BOOL);
+      assert(result_val.data != NULL);
+      bool* result = (bool*)result_val.data;
+      if (!*result) {
+        continue;
       }
-      ++next_index;
-
-      // Evaluate predicate if any.
-      if (where_clause != NULL) {
-        Datum result_val = EvalExpr(where_clause, cur_tpl);
-        assert(result_val.type == T_BOOL);
-        assert(result_val.data != NULL);
-        bool* result = (bool*)result_val.data;
-        if (!*result) {
-          continue;
-        }
-      }
-
-      ListCell* lc = NULL;
-      FOR_EACH(lc, assign_exprs) {
-        NAssignExpr* assign_expr = (NAssignExpr*)lc->data;
-        assert(assign_expr->type == NASSIGN_EXPR);
-        assert(assign_expr->column != NULL);
-        assert(assign_expr->value_expr != NULL);
-
-        NIdentifier* col = (NIdentifier*)assign_expr->column;
-        assert(col->type == NIDENTIFIER);
-        assert(col->identifier != NULL);
-
-        Datum* data = GetCol(cur_tpl, col->identifier);
-        assert(data != NULL);
-        Datum updated_value = EvalExpr(assign_expr->value_expr, cur_tpl);
-        assert(updated_value.type == data->type);
-        *data = updated_value;
-      }
-      return CopyTuple(cur_tpl);
     }
 
-    return NULL;
+    // Column projections.
+    Tuple* result_tpl = NULL;
+    CharPtrVecIt target = NULL;
+    VEC_FOREACH(target, state->target_list) {
+      Datum* data = GetCol(cur_tpl, *target);
+      assert(data != NULL);
+      result_tpl = SetCol(result_tpl, *target, *data);
+    }
+    return result_tpl;
   }
-  void Close() {}
-};
+}
+
+Tuple* InsertScan(ScanState* state) {
+  Tuple* cur_tuple = NULL;
+  for (size_t i = 0; i < arrlen(state->tuples); ++i) {
+    cur_tuple = state->tuples[i];
+    assert(cur_tuple != NULL);
+    InsertTuple(CopyTuple(cur_tuple));
+  }
+  return NULL;
+}
+
+Tuple* DeleteScan(ScanState* state) {
+  for (;;) {
+    Tuple* cur_tpl = GetTuple(state->next_index);
+    if (cur_tpl == NULL) {
+      return NULL;
+    }
+
+    // Evaluate predicate if any.
+    if (state->where_clause != NULL) {
+      Datum result_val = EvalExpr(state->where_clause, cur_tpl);
+      assert(result_val.type == T_BOOL);
+      assert(result_val.data != NULL);
+      bool* result = (bool*)result_val.data;
+      if (!*result) {
+        ++state->next_index;
+        continue;
+      }
+    }
+
+    Tuple* new_tuple = CopyTuple(cur_tpl);
+    DeleteHeapTuple(state->next_index);
+    return new_tuple;
+  }
+
+  return NULL;
+}
+
+Tuple* UpdateScan(ScanState* state) {
+  for (;;) {
+    Tuple* cur_tpl = GetTuple(state->next_index);
+    if (cur_tpl == NULL) {
+      return NULL;
+    }
+    ++state->next_index;
+
+    // Evaluate predicate if any.
+    if (state->where_clause != NULL) {
+      Datum result_val = EvalExpr(state->where_clause, cur_tpl);
+      assert(result_val.type == T_BOOL);
+      assert(result_val.data != NULL);
+      bool* result = (bool*)result_val.data;
+      if (!*result) {
+        continue;
+      }
+    }
+
+    ListCell* lc = NULL;
+    FOR_EACH(lc, state->assign_exprs) {
+      NAssignExpr* assign_expr = (NAssignExpr*)lc->data;
+      assert(assign_expr->type == NASSIGN_EXPR);
+      assert(assign_expr->column != NULL);
+      assert(assign_expr->value_expr != NULL);
+
+      NIdentifier* col = (NIdentifier*)assign_expr->column;
+      assert(col->type == NIDENTIFIER);
+      assert(col->identifier != NULL);
+
+      Datum* data = GetCol(cur_tpl, col->identifier);
+      assert(data != NULL);
+      Datum updated_value = EvalExpr(assign_expr->value_expr, cur_tpl);
+      assert(updated_value.type == data->type);
+      *data = updated_value;
+    }
+    return CopyTuple(cur_tpl);
+  }
+
+  return NULL;
+}
 
 typedef struct PlanState {
   CharPtrVec* target_list;
-  Plan* plan;
+  ScanState* scan_state;
 } PlanState;
 
 PlanState PlanQuery(Query* query) {
@@ -365,28 +322,32 @@ PlanState PlanQuery(Query* query) {
   PlanState plan_state;
   switch (query->cmd) {
     case CMD_SELECT: {
-      Plan* plan = new SequentialScan(query->target_list, query->where_clause);
       plan_state.target_list = query->target_list;
-      plan_state.plan = plan;
+      plan_state.scan_state =
+          MakeScanState(query->target_list, query->where_clause, query->values,
+                        query->assign_expr_list, SequentialScan);
       break;
     }
     case CMD_INSERT: {
-      Plan* plan = new InsertScan(query->values);
       // TODO(ryan): This is also wonky.
       plan_state.target_list = query->target_list;
-      plan_state.plan = plan;
+      plan_state.scan_state =
+          MakeScanState(query->target_list, query->where_clause, query->values,
+                        query->assign_expr_list, InsertScan);
       break;
     }
     case CMD_DELETE: {
-      Plan* plan = new DeleteScan(query->where_clause);
       plan_state.target_list = NULL;
-      plan_state.plan = plan;
+      plan_state.scan_state =
+          MakeScanState(query->target_list, query->where_clause, query->values,
+                        query->assign_expr_list, DeleteScan);
       break;
     }
     case CMD_UPDATE: {
-      Plan* plan = new UpdateScan(query->assign_expr_list, query->where_clause);
       plan_state.target_list = NULL;
-      plan_state.plan = plan;
+      plan_state.scan_state =
+          MakeScanState(query->target_list, query->where_clause, query->values,
+                        query->assign_expr_list, UpdateScan);
       break;
     }
     default:
@@ -404,11 +365,11 @@ Result execute_plan(PlanState& plan_state) {
   Result results;
   results.columns = plan_state.target_list;
   results.tuples = NULL;
-  Plan* plan = plan_state.plan;
-  Tuple* mtuple = plan->GetNext();
+  ScanState* scan_state = plan_state.scan_state;
+  Tuple* mtuple = scan_state->scan_func(scan_state);
   while (mtuple != NULL) {
     arrpush(results.tuples, mtuple);
-    mtuple = plan->GetNext();
+    mtuple = scan_state->scan_func(scan_state);
   }
   return results;
 }
