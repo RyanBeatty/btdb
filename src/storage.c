92b8eada6a689a01ffa19b4f36110e7e74b5a959
Ryan Beatty <rvbeatty@email.wm.edu>
2020-06-16 20:05:27 -0700

    cleanup

--- src/storage.c
+++ src/storage.c
@@ -223,9 +223,6 @@ void UpdateTuple(size_t table_index, Tuple* tuple, size_t index) {
     return;
   }
 
-  Tuple* old_tuple = GetTuple(table_index, index);
-  assert(old_tuple != NULL);
-  old_tuple = tuple;
   Tables[table_index][index] = tuple;
   return;
 }
