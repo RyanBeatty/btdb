a6d6ec39ce977c373fb395887c329a6a1b6bd872
Ryan Beatty <rvbeatty@email.wm.edu>
2021-03-19 16:45:54 -0700

    add test for insertion at front of page. currently failing

--- test/e2e/btdb_test.py
+++ test/e2e/btdb_test.py
@@ -795,6 +795,46 @@ def test_create_index_page_splits_in_order():
         proc.communicate()
         assert False
 
+    assert not err
+    assert output == bytes("\n".join(expected_output), encoding="utf8")
+
+    proc.kill()
+
+
+def test_create_index_page_splits_reverse_order():
+    """
+    Verify that we can create an index on a table who's data will take up more than a single page.
+    Note that we only test situations where the next tuple to be inserted occurs at the end of
+    the page.
+    """
+    proc = _start_btdb_process()
+
+    input_cmds = []
+    expected_output = ["Starting btdb"]
+    input_cmds.append('create table c (d text);\n')
+    expected_output.append('btdb> UTILITY DONE')
+    # Insertion is in the reverse order, so each new tuple should be inserted at front of page.
+    for i in range(10 ** 15 + 120 - 1, 10 ** 15 -1, -1):
+        input_cmds.append(f"insert into c (d) values ('{str(i)}');\n")
+        expected_output.append("btdb>     d")
+        expected_output.append("===============")
+    input_cmds.append("create index on c (d);\n")
+    expected_output.append('btdb> UTILITY DONE')
+    input_cmds.append("select d from c where d > '1';")
+    expected_output.append("btdb>     d")
+    expected_output.append("===============")
+    for i in range(10 ** 15, 10 ** 15 + 120):
+        expected_output.append(f"{str(i)}\t")
+    expected_output.append("btdb> Shutting down btdb\n")
+    try:
+        output, err = proc.communicate(
+            input=bytes("".join(input_cmds), encoding="utf-8"), timeout=TIMEOUT
+        )
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        proc.communicate()
+        assert False
+
     print(output)
     assert not err
     assert output == bytes("\n".join(expected_output), encoding="utf8")
